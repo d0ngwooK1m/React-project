3fde4ff92b15af45609082a815d1041919db66f1
d0ngwooK1m <dongwookim.dev@gmail.com>
2021-09-29 20:45:19 +0900

    3d.js 예제1번

--- 2nd-mini-project/src/Graph.js
+++ 2nd-mini-project/src/Graph.js
@@ -5,19 +5,63 @@ const Graph = () => {
     const contextRef = useRef(null);
 
     const [ctx, setCtx] = useState();
+    const [num1, changeNum1] = useState(50);
+
 
     useEffect(() => {
         const canvas = canvasRef.current;
 
-        const context = canvas.getContext('2d');
-        contextRef.current = context;
+        const ctx = canvas.getContext('2d');
+        contextRef.current = ctx;
         setCtx(contextRef.current);
-    }, []);
 
+        const drawHexagon = (x, y) => {
+            const a = 2 * Math.PI / 6;
+            const r = 100;
+            ctx.beginPath();
+            for (let i = 0; i < 6; i++) {
+                ctx.lineTo(x + r * Math.cos(a * i), y + r * Math.sin(a * i));
+            }
+            ctx.closePath();
+            ctx.stroke();
+            console.log(ctx);
+        };
+        drawHexagon(100, 100);
+
+        const drawGraph = (r) => {
+            const a = 2 * Math.PI / 6;
+            const x = 100;
+            const y = 100;
+            ctx.beginPath();
+            for (let i = 0; i < 6; i++) {
+                ctx.lineTo(x + r * Math.cos(a * i), y + r * Math.sin(a * i));
+            }
+            ctx.closePath();
+            ctx.stroke();
+            console.log(ctx);
+        };
+        drawGraph(num1);
+
+    }, [num1]);
+    
     console.log("contextRef :", ctx);
 
     return (
-        <canvas width="600" height="600" ref={canvasRef}></canvas>
+        <div>
+            <canvas width="600" height="600" ref={canvasRef}></canvas>
+            <button onClick={() => {
+                changeNum1(num1 + 10);
+                console.log(num1);
+                if (num1 === 100) return; 
+            }}
+            >+</button>
+            <button onClick={() => {
+                changeNum1(num1 - 10);
+                console.log(num1);
+                if (num1 === 0) return; 
+            }}
+            >-</button>
+        </div>
     );
 };
 
