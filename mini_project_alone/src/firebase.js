5ce22a8b5f2570243cbbdc901f8938529742f160
d0ngwooK1m <dongwookim.dev@gmail.com>
2021-10-05 11:28:34 +0900

    update firebase

--- mini_project_alone/src/firebase.js
+++ mini_project_alone/src/firebase.js
@@ -1,14 +1,16 @@
-import {initializeApp} from "firebase/app";
-import {getFirestore} from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+require('dotenv').config();
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD8omf3EejqqdBLh8zR_qRNcQuERh8Tqvc",
-    authDomain: "mini-project-alone.firebaseapp.com",
-    projectId: "mini-project-alone",
-    storageBucket: "mini-project-alone.appspot.com",
-    messagingSenderId: "245120232781",
-    appId: "1:245120232781:web:b8fa8aaa4741b879549b04",
-    measurementId: "G-XRWWCC5N22"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId
 }
 
 initializeApp(firebaseConfig);
